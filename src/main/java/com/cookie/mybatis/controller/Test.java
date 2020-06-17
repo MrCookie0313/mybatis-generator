66929cb62a9110ed1f5d7f29709349aa30acf8a3
sunbing_vendor <sunbing_vendor@sensetime.com>
2020-06-17 18:56:49 +0800

    第一次提交

---
+++ src/main/java/com/cookie/mybatis/controller/Test.java
@@ -0,0 +1,12 @@
+package com.cookie.mybatis.controller;
+
+/**
+ * @author sunBing
+ * @version 1.0
+ * @date 2020/6/17 0017
+ */
+public class Test {
+    public static void main(String[] args) {
+        String str1= "1";
+    }
+}
