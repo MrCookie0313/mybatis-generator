f3e4051282388ab6371c38f657305d76a3d4be9e
sunbing_vendor <sunbing_vendor@sensetime.com>
2020-06-18 11:52:56 +0800

    代码生成器测试

--- src/main/java/com/cookie/mybatis/controller/TestController.java
+++ src/main/java/com/cookie/mybatis/controller/TestController.java
@@ -1,17 +1,20 @@
 package com.cookie.mybatis.controller;
 
+import com.cookie.mybatis.mapper.OrderMapper;
 import com.cookie.mybatis.mapper.RunoobMapper;
+import com.cookie.mybatis.model.Order;
 import com.cookie.mybatis.model.Runoob;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @Autowired(required = false)
     private RunoobMapper runoobMapper;
+
+    @Autowired(required = false)
+    private OrderMapper orderMapper;
 
     @RequestMapping(value = "/getRunoob", method = RequestMethod.GET)
     public Runoob getDetail(Integer id) {
@@ -22,5 +25,10 @@ public class TestController {
     @RequestMapping(value = "/saveRunoob", method = RequestMethod.POST)
     public Boolean getDetail(@RequestBody Runoob runoob) {
         return runoobMapper.insertSelective(runoob) >= 1 ? true : false;
+    }
+
+    @RequestMapping(value = "/getAllInfo", method = RequestMethod.GET)
+    public Order getAllInfo(String id) {
+        return orderMapper.selectByPrimaryKey(id);
     }
 }
--- src/main/java/com/cookie/mybatis/model/Order.java
+++ src/main/java/com/cookie/mybatis/model/Order.java
@@ -1,7 +1,10 @@
 package com.cookie.mybatis.model;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class Order {
     private String orderId;
 
@@ -32,154 +35,11 @@ public class Order {
     private String evisaDownloadUrl;
 
     private String deletedFlag;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createdAt;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updatedAt;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date deletedAt;
 
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId == null ? null : orderId.trim();
-    }
-
-    public Integer getSkuId() {
-        return skuId;
-    }
-
-    public void setSkuId(Integer skuId) {
-        this.skuId = skuId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId == null ? null : userId.trim();
-    }
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId == null ? null : tenantId.trim();
-    }
-
-    public String getTranNo() {
-        return tranNo;
-    }
-
-    public void setTranNo(String tranNo) {
-        this.tranNo = tranNo == null ? null : tranNo.trim();
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus == null ? null : orderStatus.trim();
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode == null ? null : countryCode.trim();
-    }
-
-    public String getCurrencyCode() {
-        return currencyCode;
-    }
-
-    public void setCurrencyCode(String currencyCode) {
-        this.currencyCode = currencyCode == null ? null : currencyCode.trim();
-    }
-
-    public String getUnits() {
-        return units;
-    }
-
-    public void setUnits(String units) {
-        this.units = units == null ? null : units.trim();
-    }
-
-    public Integer getNacos() {
-        return nacos;
-    }
-
-    public void setNacos(Integer nacos) {
-        this.nacos = nacos;
-    }
-
-    public String getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public void setPaymentStatus(String paymentStatus) {
-        this.paymentStatus = paymentStatus == null ? null : paymentStatus.trim();
-    }
-
-    public String getVisaType() {
-        return visaType;
-    }
-
-    public void setVisaType(String visaType) {
-        this.visaType = visaType == null ? null : visaType.trim();
-    }
-
-    public String getVerificationRemark() {
-        return verificationRemark;
-    }
-
-    public void setVerificationRemark(String verificationRemark) {
-        this.verificationRemark = verificationRemark == null ? null : verificationRemark.trim();
-    }
-
-    public String getEvisaDownloadUrl() {
-        return evisaDownloadUrl;
-    }
-
-    public void setEvisaDownloadUrl(String evisaDownloadUrl) {
-        this.evisaDownloadUrl = evisaDownloadUrl == null ? null : evisaDownloadUrl.trim();
-    }
-
-    public String getDeletedFlag() {
-        return deletedFlag;
-    }
-
-    public void setDeletedFlag(String deletedFlag) {
-        this.deletedFlag = deletedFlag == null ? null : deletedFlag.trim();
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Date getDeletedAt() {
-        return deletedAt;
-    }
-
-    public void setDeletedAt(Date deletedAt) {
-        this.deletedAt = deletedAt;
-    }
 }
