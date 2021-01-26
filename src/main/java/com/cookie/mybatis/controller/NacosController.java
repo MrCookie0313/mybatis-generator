c7e9f0d168031daec4c19c2d6f35ba0f4dbb84ab
sunbing_vendor <sunbing_vendor@sensetime.com>
2021-01-26 12:01:49 +0800

    nacos配置中心实现

---
+++ src/main/java/com/cookie/mybatis/controller/NacosController.java
@@ -0,0 +1,29 @@
+package com.cookie.mybatis.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Cookie
+ * @version 1.0
+ * @date 2021/01/26 11:41
+ */
+@RestController
+@RefreshScope
+public class NacosController {
+
+    @Value("${config.user.name}")
+    private String userName;
+
+    @Value("${config.user.age}")
+    private Integer age;
+
+    @GetMapping("/get")
+    public String getUserName(){
+        String result= "我是"+userName+"今年"+age+"岁";
+        return result;
+    }
+
+}
