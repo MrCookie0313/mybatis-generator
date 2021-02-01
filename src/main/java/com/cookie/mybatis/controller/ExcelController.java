497b29402099e15d22def2142f90614385b952ae
sunbing_vendor <sunbing_vendor@sensetime.com>
2021-02-01 19:29:50 +0800

    Springboot 基于swagger 整合Knife4j

--- src/main/java/com/cookie/mybatis/controller/ExcelController.java
+++ src/main/java/com/cookie/mybatis/controller/ExcelController.java
@@ -1,12 +1,12 @@
 package com.cookie.mybatis.controller;
 
 import com.cookie.mybatis.service.ExcelService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  * @version 1.0
  * @date 2020/09/15 18:51
  */
+@Api(tags = "hello word")
 @Controller
 @RequestMapping("/excel")
 public class ExcelController {
